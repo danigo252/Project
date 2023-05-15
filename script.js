5738f63f75682d771faac9b1a9acdffe2c6d770b
daniel <danielgonzalez252@gmail.com>
2023-05-15 04:03:31 -0400

    Declaration and use of .innerHTML

--- script.js
+++ script.js
@@ -8,26 +8,31 @@ const holidaysContainer = document.getElementById("holidays-container");
 const showHolidaysBtn = document.getElementById("show-holidays-btn");
 
 //create of the function .addEvenrListener
-showHolidaysBtn.addEventListener("click", () =>  {
+showHolidaysBtn.addEventListener("click", () =>
+{
     // function fetch to get the apiUrl
     fetch(apiUrl)
         // We get the response with the Json Data
         .then(response => response.json())
-        .then(data => {
+        .then(data =>
+        {
             // Created const holidays to get all the data in just a variable
             const holidays = data;
             let html = "";
             //We use .forEach to separete elements from data
-            holidays.forEach(holiday => {
+            holidays.forEach(holiday =>
+            {
                 //Everytime we get a new element,it creates a render of the data
                 html += `
-         <div class="holiday">
-           <h2>${holiday.name}</h2>
-           <p>Date: ${holiday.date}</p>
-           <p>Type: ${holiday.type}</p>
-         </div>
-       `;
+                 <div class="holiday">
+                   <h2>${holiday.name}</h2>
+                   <p>Date: ${holiday.date}</p>
+                   <p>Type: ${holiday.type}</p>
+                 </div>`;
             });
+            //From the data we get the element .name , .data and .type from the JSON
+
+            //We use .innerHTML to generate dinamy HTML content
             holidaysContainer.innerHTML = html;
             showHolidaysBtn.style.display = "none";
         })
