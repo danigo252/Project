7f734b956f8b64660c1ec0af910b960a15b94443
daniel <danielgonzalez252@gmail.com>
2023-05-15 03:41:15 -0400

    comment about the declaration of the const apiUrl that will have the API that will be used

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// const apiUrl with the link to the API that will be used
 const apiUrl = "https://date.nager.at/api/v2/publicholidays/2020/US";
 const holidaysContainer = document.getElementById("holidays-container");
 const showHolidaysBtn = document.getElementById("show-holidays-btn");
