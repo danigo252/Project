a9ead2b168eaed814306ea50857b2b5f748be380
daniel <danielgonzalez252@gmail.com>
2023-05-15 03:12:18 -0400

    We create a showHolidaysBtn cons to cast show-holidays-btn, declared inside index.html on line 15

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 const apiUrl = "https://date.nager.at/api/v2/publicholidays/2020/US";
 const holidaysContainer = document.getElementById("holidays-container");
+const showHolidaysBtn = document.getElementById("show-holidays-btn");
