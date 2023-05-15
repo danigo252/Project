04d8ec76ba859c65fadf8f4d52d753ab2cbd1274
daniel <danielgonzalez252@gmail.com>
2023-05-15 03:20:20 -0400

    We create the event listener that will be trigger when the final user hit the click button,then we will have a Json Response with the data inside, from the data we received we will use .forEach to separate objects and attributes from each other, after that we create a div with the class holiday to be render each time we jump from one object to other until there is no more objects to be cast, at the end we create a catch error just in case we get a bad response from the API making impossible to get the data

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const apiUrl = "https://date.nager.at/api/v2/publicholidays/2020/US";
 const holidaysContainer = document.getElementById("holidays-container");
 const showHolidaysBtn = document.getElementById("show-holidays-btn");
+
+showHolidaysBtn.addEventListener("click", () => {
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const holidays = data;
+            let html = "";
+            holidays.forEach(holiday => {
+                html += `
+         <div class="holiday">
+           <h2>${holiday.name}</h2>
+           <p>Date: ${holiday.date}</p>
+           <p>Type: ${holiday.type}</p>
+         </div>
+       `;
+            });
