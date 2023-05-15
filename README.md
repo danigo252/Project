6899df0523c63b91517bbaa4f071ba98708328b8
daniel <danielgonzalez252@gmail.com>
2023-05-15 03:24:21 -0400

    Finishing the README.md

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Project
-Flatiron Project
+US Public Holidays 2020
 
-
+The main use of this Webpage is to get all the holidays from US 2020
 
 ## API address used
 
--- script.js
+++ script.js
@@ -2,10 +2,14 @@ const apiUrl = "https://date.nager.at/api/v2/publicholidays/2020/US";
 const holidaysContainer = document.getElementById("holidays-container");
 const showHolidaysBtn = document.getElementById("show-holidays-btn");
 
+
+
 showHolidaysBtn.addEventListener("click", () => {
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
+
+
             const holidays = data;
             let html = "";
             holidays.forEach(holiday => {
@@ -17,3 +21,11 @@ showHolidaysBtn.addEventListener("click", () => {
          </div>
        `;
             });
+            holidaysContainer.innerHTML = html;
+            showHolidaysBtn.style.display = "none";
+        })
+        .catch(error => {
+            console.error(error);
+            holidaysContainer.innerHTML = "An error occurred while fetching data.";
+        });
+});
